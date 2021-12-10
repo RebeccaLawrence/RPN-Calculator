42ee3df691ecf1e9e2f5afc67ac226f73dee0bc0
rebecca_lawrence <rlaw01@bu.edu>
2021-12-10 01:26:56 -0500

    inital push

--- node.c
+++ node.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //
 // Created by rebec on 11/17/2021.
 //
@@ -28,4 +29,36 @@ node *createNode(double value, int type) {
     }
     return newNode;
 
+=======
+//
+// Created by rebec on 11/17/2021.
+//
+
+// taken from ps3 solutions given
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "node.h"
+
+enum type {operator, number};
+
+
+node *createNode(double value, int type) {
+    node *newNode = (struct node*) malloc(sizeof(node));
+    if (newNode) {
+        newNode->value = value;
+        newNode->next = NULL;
+        if (type == 0){
+            newNode->type = operator;
+        } else {
+            newNode->type = number;
+        }
+    } else {
+        puts("Unable to allocate memory for a node.\n");
+        exit(-1);
+    }
+    return newNode;
+
+>>>>>>> 63a7e8a2023f2c3e4d819eab6f4cf94f046d9576
 }
--- rpn .h
+++ rpn .h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //
 // Created by rebec on 11/17/2021.
 //
@@ -9,3 +10,16 @@ double evaluate (char* expression, int* status); // evaluates mathematical expre
 double recursivePower(double , double);           //calculates the power of two doubles recursively
 
 #endif //PS4_RPN_H
+=======
+//
+// Created by rebec on 11/17/2021.
+//
+
+#ifndef PS4_RPN_H
+#define PS4_RPN_H
+
+double evaluate (char* expression, int* status); // evaluates mathematical expressions
+double recursivePower(double , double);           //calculates the power of two doubles recursively
+
+#endif //PS4_RPN_H
+>>>>>>> 63a7e8a2023f2c3e4d819eab6f4cf94f046d9576
